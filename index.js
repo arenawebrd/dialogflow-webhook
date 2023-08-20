26707bb0a4e788ea7a32ffc04b3f6aa857786790
Arenaweb RD <67570426+arenawebrd@users.noreply.github.com>
2023-08-20 01:35:26 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { WebhookClient } = require("dialogflow-fulfillment");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/webhook", (req, res) => {
+  const agent = new WebhookClient({ request: req, response: res });
+
+  function welcome(agent) {
+    agent.add("¡Hola! ¿En qué puedo ayudarte?");
+  }
+
+  function fallback(agent) {
+    agent.add("Lo siento, no puedo responder a eso en este momento.");
+  }
+
+  let intentMap = new Map();
+  intentMap.set("Default Welcome Intent", welcome);
+  intentMap.set("Default Fallback Intent", fallback);
+
+  agent.handleRequest(intentMap);
+});
+
+app.listen(3300, () => {
+  console.log("Servidor en ejecución en el puerto 3300");
+});
