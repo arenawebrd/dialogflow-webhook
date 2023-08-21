11820a8fb19a3014e56411591d04033553aed405
Arenaweb RD <67570426+arenawebrd@users.noreply.github.com>
2023-08-21 02:56:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,46 @@
 const express = require("express");
-const app = express();
+const axios = require ("axios");
+const bodyParser = require("body-parser");
 const { WebhookClient } = require("dialogflow-fulfillment");
 
-app.get("/", function (req, res) {
-  res.send("Hola Mundo");
-});
+const app = express();
+app.use(bodyParser.json());
 
-app.post("/webhook", express.json(), function (req, res) {
+app.post("/webhook", (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
-  console.log("Dialogflow Request headers: " + JSON.stringify(req.headers));
-  console.log("Dialogflow Request body: " + JSON.stringify(req.body));
 
-  // FUNCION PARA SACAR TIEMPO
-  let date = new Date();
-  let data = date.toLocaleString('es', {timeZone: "America/Santo_Domingo", hour: 'numeric', hour12: false });
-   
-      // FUNCIÓN SALUDOS AVANZADA
-    function welcome(agent) {
-    if(data >=5 && data <=11)
-    agent.add('Hola 🤖 Buen dia! ' +'\n'+'\n'+
-             'Bienvenido(a) soy *Ana Bot*, escoja una opcion de abajo!' +'\n'+'\n'+
-             '[1] Quiero saber mas'+'\n'+
-             '[2] Asistir a un Curso Gratis'+'\n'+
-             '[3] Entrar al Grupo Vip'+'\n'+
-             '[4] Regalo Sorpresa');
-    
-    else if(data >=12 && data <=17)
-      agent.add('Hola 🤖 Buenas tardes!' +'\n'+'\n'+
-            'Bienvenido(a) soy *Ana Bot*, escoja una opcion de abajo!' +'\n'+'\n'+
-            '[1] Quiero saber mas'+'\n'+
-            '[2] Asistir a un Curso Gratis'+'\n'+
-            '[3] Entrar al Grupo Vip'+'\n'+
-            '[4] Regalo Sorpresa');
-    else 
-      agent.add('Hola 🤖 Buenas noche!' +'\n'+'\n'+
-            'Bienvenido(a) soy *Ana Bot*, escoja una opcion de abajo!' +'\n'+'\n'+
-            '[1] Quiero saber mas'+'\n'+
-            '[2] Asistir a un Curso Gratis'+'\n'+
-            '[3] Entrar al Grupo Vip'+'\n'+
-            '[4] Regalo Sorpresa');
-  }  
+  function welcome(agent) {
+    agent.add("¡Hola! ¿En qué puedo ayudarte?");
+  }
 
   function fallback(agent) {
-    agent.add(`No entendí`);
-    agent.add(`Lo siento, ¿Puedes volver a intentarlo?`);
+    agent.add("Lo siento, no puedo responder a eso en este momento.");
   }
-  function ProbandoWebhook(agent) {
-    for (let i = 1; i <= 5; i++) {
-      agent.add(`Esta es la respuesta: ` + i);
-    }
-  }
+
+ if(intentName == 'Consultar'){
+ var Consulta = request.body.queryResult.parameters['Consulta'];
+ 
+ return axios.get("https://api.steinhq.com/v1/storages/634366c5d27cdd09f0c3c8a6/Citas").then(res => {
+ res.data.map(person => {
+ if (person.Consulta === Consulta)
+ response.json({"fulfillmentText" :"Detalles de la consulta "+Consulta+":"+"\n"+
+ "Nombre: "+person.Nombre+"\n"+ 
+ "Fecha: "+person.Fecha});
+ });
+ }); 
+ }
+
+
+
+  
   let intentMap = new Map();
   intentMap.set("Default Welcome Intent", welcome);
+  intentMap.set("Consultar", consulta);
   intentMap.set("Default Fallback Intent", fallback);
-  intentMap.set("ProbandoWebhook", ProbandoWebhook);
-  // intentMap.set('your intent name here', yourFunctionHandler);
-  // intentMap.set('your intent name here', googleAssistantHandler);
+
   agent.handleRequest(intentMap);
 });
 
-let port = 3300;
-app.listen(port, () => {
-  console.log("Estamos ejecutando el servidor en el puerto " + port);
+app.listen(3300, () => {
+  console.log("Servidor en ejecución en el puerto 3300");
 });
