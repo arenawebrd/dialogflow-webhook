acd7145af41ff1b29b12b7f0e139931d3345abf4
Arenaweb RD <67570426+arenawebrd@users.noreply.github.com>
2023-08-21 04:02:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,19 +12,6 @@ app.post("/webhook", (req, res) => {
     agent.add("¡Hola! ¿En qué puedo ayudarte?");
   }
 
- if(intentName == 'Consultar'){
- var Consulta = request.body.queryResult.parameters['Consulta'];
- 
- return axios.get("https://api.steinhq.com/v1/storages/634366c5d27cdd09f0c3c8a6/Citas").then(res => {
- res.data.map(person => {
- if (person.Cedula === Cedula)
- response.json({"fulfillmentText" :"Detalles de la cedula "+Cedula+":"+"\n"+
- "Nombre: "+person.Nombre+"\n"+ 
- "Fecha: "+person.Fecha});
- });
- }); 
- }
-
   function fallback(agent) {
     agent.add("Lo siento, no puedo responder a eso en este momento.");
   }
