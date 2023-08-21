e65a898bd8489072939c394944400db2572f3533
Arenaweb RD <67570426+arenawebrd@users.noreply.github.com>
2023-08-21 03:52:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require ("axios");
 const bodyParser = require("body-parser");
 const { WebhookClient } = require("dialogflow-fulfillment");
 
@@ -12,12 +13,26 @@ app.post("/webhook", (req, res) => {
     agent.add("¡Hola! ¿En qué puedo ayudarte?");
   }
 
+ if(intentName == 'Consultar'){
+ var Consulta = request.body.queryResult.parameters['Consulta'];
+ 
+ return axios.get("https://api.steinhq.com/v1/storages/634366c5d27cdd09f0c3c8a6/Citas").then(res => {
+ res.data.map(person => {
+ if (person.Cedula === Cedula)
+ response.json({"fulfillmentText" :"Detalles de la cedula "+Cedula+":"+"\n"+
+ "Nombre: "+person.Nombre+"\n"+ 
+ "Fecha: "+person.Fecha});
+ });
+ }); 
+ }
+
   function fallback(agent) {
     agent.add("Lo siento, no puedo responder a eso en este momento.");
   }
 
   let intentMap = new Map();
   intentMap.set("Default Welcome Intent", welcome);
+   intentMap.set("Consultar", consultar);
   intentMap.set("Default Fallback Intent", fallback);
 
   agent.handleRequest(intentMap);
