056aab9f17bea7bd53638202c0be7318286da0e1
Arenaweb RD <67570426+arenawebrd@users.noreply.github.com>
2023-08-23 21:16:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { WebhookClient } = require("dialogflow-fulfillment");
+const mysql = require('mysql');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post("/webhook", (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
+
+  var connection = mysql.createConnection({ 
+   host: process.env.MYSQL_HOST, 
+   user: process.env.MYSQL_USER, 
+   password: process.env.MYSQL_PASS, 
+   database: process.env.MYSQL_DB 
+   }); 
+   connection.connect(); 
+
 
   function welcome(agent) {
     agent.add("¡Hola! ¿En qué puedo ayudarte?");
