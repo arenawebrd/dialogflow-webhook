e0391028aa9839c81ca513e0874314f9e160f3ef
Arenaweb RD <67570426+arenawebrd@users.noreply.github.com>
2023-08-24 01:47:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,45 +24,45 @@ connection.connect((error) => {
 app.post("/webhook", (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
 
-  function welcome(agent) {
-    agent.add("¡Hola! ¿En qué puedo ayudarte?");
-  }
+function welcome(agent) {
+  agent.add("¡Hola! ¿En qué puedo ayudarte?");
+}
 
-  function fallback(agent) {
-    agent.add("Lo siento, no puedo responder a eso en este momento.");
-  }
+function fallback(agent) {
+  agent.add("Lo siento, no puedo responder a eso en este momento.");
+}
 
-  function insertData(agent) {
-    const Nombre = agent.parameters.Nombre;
-    const Telefono = agent.parameters.Telefono;
-    const Cedula = agent.parameters.Cedula;
+function insertData(agent) {
+  const nombre = agent.parameters.nombre;
+  const telefono = agent.parameters.telefono;
+  const cedula = agent.parameters.cedula;
 
-    const query =
-      "INSERT INTO Registro (Nombre, Telefono, Cedula) VALUES (?, ?, ?)";
-    connection.query(
-      query,
-      [Nombre, Telefono, Cedula],
-      (error, results) => {
-        if (error) {
-          console.error("Error al insertar datos: ", error);
-          agent.add("Hubo un error al insertar los datos.");
-        } else {
-          console.log("Datos insertados correctamente");
-          agent.add(
-            "Los datos se han insertado correctamente en la base de datos."
-          );
-        }
+  const query =
+    "INSERT INTO Registro (nombre, telefono, cedula) VALUES (?, ?, ?)";
+  connection.query(
+    query,
+    [nombre, telefono, cedula],
+    (error, results) => {
+      if (error) {
+        console.error("Error al insertar datos: ", error);
+        agent.add("Hubo un error al insertar los datos.");
+      } else {
+        console.log("Datos insertados correctamente");
+        agent.add(
+          "Los datos se han insertado correctamente en la base de datos."
+        );
       }
-    );
-  }
+    }
+  );
+}
 
-  let intentMap = new Map();
-  intentMap.set("Default Welcome Intent", welcome);
-  intentMap.set("Default Fallback Intent", fallback);
-  intentMap.set("Insert Data Intent", insertData);
+let intentMap = new Map();
+intentMap.set("Default Welcome Intent", welcome);
+intentMap.set("Default Fallback Intent", fallback);
+intentMap.set("Insert Data", insertData);
 
-  agent.handleRequest(intentMap);
-});
+agent.handleRequest(intentMap);
+
 
 app.listen(3300, () => {
   console.log("Servidor en ejecución en el puerto 3300");
