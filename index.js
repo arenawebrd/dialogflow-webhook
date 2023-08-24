4ae828e0189103189af83c1a648bf0bdcd99cf2a
Arenaweb RD <67570426+arenawebrd@users.noreply.github.com>
2023-08-24 01:23:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post("/webhook", (req, res) => {
   let intentMap = new Map();
   intentMap.set("Default Welcome Intent", welcome);
   intentMap.set("Default Fallback Intent", fallback);
-  intentMap.set("RegistrarContacto", registrar contacto);
+  intentMap.set("RegistrarContacto", registrar);
 
   agent.handleRequest(intentMap);
 });
