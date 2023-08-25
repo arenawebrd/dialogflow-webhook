3700b265f9a33909e5bcf38e48ba64efc9c29832
Arenaweb RD <67570426+arenawebrd@users.noreply.github.com>
2023-08-25 06:19:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,98 +30,20 @@ app.post("/webhook", (req, res) => {
     });
   }
   
-  function queryDatabase(connection){
-    return new Promise((resolve, reject) => {
-      connection.query('YOUR_SQL_QUERY', (error, results, fields) => {
-        resolve(results);
-      });
-    });
-  }
-
-   function insertIntoDatabase(connection, data){
-    return new Promise((resolve, reject) => {
-      connection.query('INSERT INTO Registro SET ?', data, (error, results, fields) => {
-        resolve(results);
-      });
-    });
-  }
-  
-  function updateDatabase(connection, data){
-    return new Promise((resolve, reject) => {
-      connection.query(`UPDATE users SET ? WHERE email = ?`, [data, data.email], (error, results, fields) => {
-        resolve(results);
-      });
-    });
-  }
-
-  function deleteFromDatabase(connection, email){
-    return new Promise((resolve, reject) => {
-      connection.query(`DELETE from users WHERE email = ?`, email, (error, results, fields) => {
-        resolve(results);
-      });
-    });
-  }
-  
-  function handleReadFromMySQL(agent){
-    const user_email = agent.parameters.email;
-    return connectToDatabase()
-    .then(connection => {
-      return queryDatabase(connection)
-      .then(result => {
-        console.log(result);
-        result.map(user => {
-          if(user_email === user.email){
-            agent.add(`First Name: ${user.first_name} and Last Name: ${user.last_name}`);
-          }
-        });        
-        connection.end();
-      });
-    });
-  }
-
-  function handleWriteIntoMysql(agent){
-    const data = {
-     nombre: "Juan",
-     telefono: "8098909090",
-     cedula: "0990030908"
-    };
-    return connectToDatabase()
-    .then(connection => {
-      return insertIntoDatabase(connection, data)
-      .then(result => {
- 		agent.add(`La informacion se ha agregado con exito`);       
-        connection.end();
-      });
-    });
-  }
-  
-  function handleUpdateMysql(agent){
-    const data = {
-      first_name: "Anshul",
-      last_name: "Random",
-      email: "sample@email.com"
-    };
-    return connectToDatabase()
-    .then(connection => {
-      return updateDatabase(connection, data)
-      .then(result => {
- 		agent.add(`Data updated`);       
-        connection.end();
-      });
-    });
-  }
-  
-  function handleDeleteFromMysql(agent){
-    const email = "sample@email.com";
-    return connectToDatabase()
-    .then(connection => {
-      return deleteFromDatabase(connection, email)
-      .then(result => {
- 		agent.add(`Data deleted`);       
-        connection.end();
-      });
-    });
-  }
+if(intentName == 'writeDataIntoMysql'){
+ console.log('Agregar Contacto')
+ 
+ var nombre = request.body.queryResult.parameters['nombre'];
+ var telefono = request.body.queryResult.parameters['telefono'];
+ var cedula = request.body.queryResult.parameters['cedula'];
+ var query = 'insert into Registro values ("'+nombre+'","'+Telefono+'","'+cedula+'")';
+ 
+ connection.query(query, function (error, results, fields) {
+ if (error) throw error;
+ connection.end();
+ response.json({"fulfillmentText" :"El contacto se ha agregado con exito!" })
+ }); 
+}
 
 
   let intentMap = new Map();
