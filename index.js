af971c473259a0d0ccce0ee50fb05ad941818f36
Arenaweb RD <67570426+arenawebrd@users.noreply.github.com>
2023-08-25 05:52:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { WebhookClient } = require("dialogflow-fulfillment");
+const mysql = require('mysql');
 
 const app = express();
 app.use(bodyParser.json());
